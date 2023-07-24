77603fffb5ed04b2f1606b4679150c2b4d074e31
devilarmaan <devilarmaan63@gmail.com>
2023-07-24 11:13:34 +0600

    add some file

---
+++ src/components/shared/clock-action/index.jsx
@@ -0,0 +1,10 @@
+const ClockAction = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ClockAction;
--- src/components/shared/clock-display/index.jsx
+++ src/components/shared/clock-display/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {format} from 'date-fns'
 
 const ClockDisplay = ({ date, title, timezone, offset }) => {
     let localOffsetHr = (offet / 60);
   return (
     <>
       <h1>Title: {title}</h1>
-      <h3>{date.toString()}</h3>
+      <h3>{format(date, "yyy-MM-dd hh:mm:ss aaaaa'm'")}</h3>
       <p>
         {timezone}{localOffsetHr < 0 ? `+${Math.abs(localOffsetHr)}` : `-${Math.abs(localOffsetHr)}`}
       </p>
